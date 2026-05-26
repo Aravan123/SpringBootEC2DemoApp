6d9cca5964a99280bb5e33e252ff79323c3257dd
Aravan123 <reddeppa0030@gmail.com>
2026-05-26 18:53:36 +0530

    first commit

---
+++ src/main/java/com/arav/SpringBootEc2DemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.arav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEc2DemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEc2DemoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arav/SpringBootEc2DemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arav;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEc2DemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
